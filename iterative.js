9336ef6a6e485d19964811f0a715a3a014aace34
Lucas Bigelow <lukejbigelow@gmail.com>
2020-09-22 13:44:21 -0500

    second problem iteratively

--- iterative.js
+++ iterative.js
@@ -2,10 +2,27 @@
 
 //ONE//
 function countSheep (num){
-    
   for( let i =0; i< num ; i++){
     console.log(`${num-i} : Another sheep jumps oner the fence`)
   }
   console.log('All sheep jumped over the fence');
 }
+
+//TWO//
+function powerCalculator (base, exponent) {
+  if (exponent >= 0) {
+    let result = 1;
+    for (let i = 0; i < exponent; i++) {
+      result *= base;
+    }
+    return result;
+  }
+  return 'exponent should be >= 0';
+  // return Math.pow(base, exponent);
+}
+
+
+//TESTING ZONE//
+
 countSheep(3);
+console.log(powerCalculator(5, 3));
