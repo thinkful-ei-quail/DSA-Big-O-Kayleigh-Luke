a28a7c65ba2f8895f425919ae6a5b1df83debd08
Lucas Bigelow <lukejbigelow@gmail.com>
2020-09-22 13:21:37 -0500

    added hanoi tower

---
+++ hanoi.js
@@ -0,0 +1,29 @@
+'use strict';
+
+// const fakeHanoi = [
+//   [3, 2, 1],
+//   [],
+//   [] 
+// ];
+
+// function towerOfHanoi (somethin) {
+
+// }
+
+function stepsToSolveHanoiT(height, srcP, desP, bufferP) {
+  if (height >= 1) {
+
+    // Move a tower of height-1 to the buffer peg, using the destination peg.
+    stepsToSolveHanoiT(height - 1, srcP, bufferP, desP);
+
+    // Move the remaining disk to the destination peg.
+    console.log('Move disk from Tower ', srcP, ' to Tower ', desP);
+
+    // Move the tower of `height-1` from the `buffer peg` to the `destination peg` using the `source peg`.        
+    stepsToSolveHanoiT(height - 1, bufferP, desP, srcP);
+  }
+  
+  return;
+}
+
+stepsToSolveHanoiT(5, "A", "C", "B");
