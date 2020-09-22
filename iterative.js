7e23c48ed545f64f61a7168bb77f9a4ecd7753c6
Kayleigh Young <64609499+kayleighkat98@users.noreply.github.com>
2020-09-22 13:35:14 -0500

    sheep

--- iterative.js
+++ iterative.js
@@ -1,1 +1,11 @@
 'use strict';
+
+//ONE//
+function countSheep (num){
+    
+  for( let i =0; i< num ; i++){
+    console.log(`${num-i} : Another sheep jumps oner the fence`)
+  }
+  console.log('All sheep jumped over the fence');
+}
+countSheep(3);
